60273e41605c948b38ccde26976147087a86a801
aditore <anthonyditore@gmail.com>
2022-05-14 08:56:57 -0700

    setting up more file structure and boiler plate server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+//add Apollo Server
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+//add typeDef and resolver
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+//Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +26,18 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+//apply Apollo Server
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  });
+}
+
+//call function
+startApolloServer(typeDefs, resolvers);
